3ccaa73be4484c94b8eac6b5a64c644c44cc43b6
Michael <nixmix800@gmail.coom>
2019-10-03 08:25:10 +0200

    first commit of basecode

---
+++ BIF-SWE1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BIF_SWE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
